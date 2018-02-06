f072c978abb4b3630aaad10b83cc6b71b8605d32
Michael Berk <michael.berk98@web.de>
2018-02-06 08:57:06 +0100

    Zweiter Versuch des Commits

--- Client/Client/Form1.cs
+++ Client/Client/Form1.cs
@@ -29,26 +29,27 @@ namespace Client
         }
 
         void Connectionload()   //In eine Methode gepackt, damit es nicht einfach im Komstruktur rumfliegt
+        {
+            mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
             {
-                mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                mySocket.Connect(new IPEndPoint(IPAddress.Parse("localhost"), 8887)); // Die Ports könnten auch gleich sein müssen sie aber nicht
 
-                try
-                {
-                    mySocket.Connect(new IPEndPoint(IPAddress.Parse("localhost"), 8887)); // Die Ports könnten auch gleich sein müssen sie aber nicht
-
-                }
-                catch (Exception)
-                {
-
-                    throw;
-                }
-
-                if (mySocket.Connected)
-                {
-                    MessageBox.Show("Connection established.");
-                    mySocket.Close();
-                }
             }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            if (mySocket.Connected)
+            {
+                MessageBox.Show("Connection established.");
+                mySocket.Close();
+            }
+
+    }
 
         private void Form1_Load(object sender, EventArgs e)
         {
